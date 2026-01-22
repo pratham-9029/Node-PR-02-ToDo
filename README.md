f089d0af199409016108e10ee34acecac53e8338
pratham-9029 <saixerox5075@gmail.com>
2026-01-22 12:57:24 +0530

    Done

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Node-PR-02-ToDo
+# day-07-crud
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.js
+```
+
+This project was created using `bun init` in bun v1.3.6. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
---
+++ index.js
@@ -0,0 +1,84 @@
+
+import express from "express"
+const app = express();
+
+const PORT = 7777;
+let users = [];
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.set('view engine', 'ejs')
+
+
+//View HTML
+app.get('/', (req, res) => {
+    return res.render('index', { users });
+})
+
+
+//Delete User
+app.get('/delete/usr/:id', (req, res) => {
+
+    users = users.filter((user) => {
+        return user.id != parseInt(req.params.id);
+    })
+
+    return res.redirect(req.get('Referrer') || '/');
+
+})
+
+
+//Edit User
+app.get('/edit/usr/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId);
+
+    if (user) {
+        return res.render('edit', { user });
+    } else {
+        return res.redirect('/');
+    }
+})
+
+
+//Add User
+app.post('/create', (req, res) => {
+
+    if ( req.body.text == '') {
+        return res.redirect(req.get('refferer') || '/')
+    } else {
+        let user = {
+            id: Date.now(),
+            text: req.body.text
+        }
+        users.push(user);
+    }
+
+    return res.redirect(req.get('Referrer') || '/');
+})
+
+//Update User
+app.post('/update', (req, res) => {
+    const userId = parseInt(req.query.id);
+    const newText = req.body.text;
+
+    const userIndex = users.findIndex(u => u.id === userId);
+
+    if (userIndex !== -1) {
+        users[userIndex].text = newText;
+    }
+
+    return res.redirect('/');
+})
+
+
+//Server Start
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Server Start");
+        console.log("http://localhost:" + PORT);
+    }
+})
